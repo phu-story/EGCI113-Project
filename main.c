932c5eef83357437ac627a695685db3912b7b9b2
phu.story <konthainakab@gmail.com>
2024-10-14 20:43:22 +0700

    patient_Regitration

--- main.c
+++ main.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
-int main(){
-    printf("Hello World");
+// Global Function
+void patient_Regitration();
+
+int main() {
+    patient_Regitration();
+
+    return 0;
+}
+
+void patient_Regitration() {
+    // Local Variable
+    char PatientName[100], fileName[20];
+    int PatientID;
+
+    printf("Enter patient's name: ");
+    scanf("%s", PatientName);
+
+    printf("Enter patient's ID number: ");
+    scanf("%d", &PatientID);
+
+    sprintf(fileName, "PatientRecord/%d.txt", PatientID);
+
+    FILE *fptr;
+    fptr = fopen(fileName, "w");
+
+    fclose(fptr);
 }
