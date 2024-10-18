d6cbe517f812df794d7126e0584ee8e22fe58f83
phu.story <konthainakab@gmail.com>
2024-10-18 19:20:19 +0700

    Improve QOL: Able to retry patient id without get back to main menu

--- patientSearch.c
+++ patientSearch.c
@@ -32,6 +32,16 @@ void patientSearch() {
 
     if(!isFound) {
         printf("Patient Id %s not found.\n", queryId);
+        printf("Press 1 to retry or 2 to return to main menu\n");
+        char retryQ = getch();
+        // printf("retryQ -> %c", retryQ);
+
+        if (retryQ == '1'){
+            clear();
+            return patientSearch();
+        } else if (retryQ == '2') {
+            return pressAnyKeyToContinue();
+        }
     }
 
     return pressAnyKeyToContinue();
